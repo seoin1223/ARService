e2cc6da8324fbe6c40b86f231c03910eae596708
seoin <seoin2389@icloud.com>
2023-11-16 15:01:54 +0900

    - PR Test 커밋

--- ARService/src/main/java/com/arservice/arservice/ArServiceApplication.java
+++ ARService/src/main/java/com/arservice/arservice/ArServiceApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ArServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ArServiceApplication.class, args);
-    }
 
+        SpringApplication.run(ArServiceApplication.class, args);
+        System.out.println("test on");
+    }
+        
 }
